bfa21d99d11c734d4d880dcff42e01ec94ad543b
Amirrr1987 <Maghami.A1987@gmail.com>
2022-01-01 22:04:51 +0330

    feat: VitePWA
    - install VitePWA package
    - import VitePWA default config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 import vue from "@vitejs/plugin-vue";
 import WindiCSS from "vite-plugin-windicss";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +14,5 @@ export default defineConfig({
       "/@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [vue(), WindiCSS()],
+  plugins: [vue(), WindiCSS(), VitePWA({})],
 });
