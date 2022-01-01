5cb8673f41e03b78cdaff67b8013476f876656c7
Amir Maghami <maghami.a1987@gmail.com>
2022-01-01 13:16:03 +0330

    feat: windiCSS config
    - change and set basic config for windiCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import path from "path";
+import vue from "@vitejs/plugin-vue";
 import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),WindiCSS(),]
-})
+  server: {
+    port: 4000,
+  },
+  resolve: {
+    alias: {
+      "/@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [vue(), WindiCSS()],
+});
--- windi.config.ts
+++ windi.config.ts
@@ -1,13 +1,18 @@
-import { defineConfig } from "vite-plugin-windicss";
+import { defineConfig } from "windicss/helpers";
 
 export default defineConfig({
   attributify: true,
-  extract: {
-    include: ["src/**/*.{vue,html,jsx,tsx}"],
-    exclude: ["node_modules", ".git"],
-  },
-  alias: {
-    btn: "w-24 h-10",
-    icon: "w-6 h-6 fill-current",
+  shortcuts: {
+    btn: "py-2 px-14 rounded shadow-md drop-shadow",
+    "btn__primary":
+      "text-white bg-purple-500 hover:bg-purple-600 focus:bg-purple-400",
+    "btn__secondary":
+      "text-white bg-pink-500 hover:bg-pink-600 focus:bg-pink-400",
+    "btn__success":
+      "text-white bg-green-500 hover:bg-green-600 focus:bg-green-400",
+    "btn__danger": "text-white bg-red-500 hover:bg-red-600 focus:bg-red-400",
+    "btn__info": "text-white bg-blue-500 hover:bg-blue-600 focus:bg-blue-400",
+    "btn__warning":
+      "text-black bg-yellow-500 hover:bg-yellow-600 focus:yellow-600 focus:bg-yellow-400",
   },
 });
