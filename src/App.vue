91da499798441ee5b3cadb7442ba2bd0fcd6b9d8
Amir Maghami <Maghami.A1987@gmail.com>
2023-09-24 14:25:22 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,57 @@
 <template>
-  {{ ip }}
+  <table>
+    <tr v-for="(item, key) in ipData">
+      <td>{{ key }}</td>
+      <td>{{ item }}</td>
+    </tr>
+  </table>
 </template>
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
-const ip = ref();
+
+export interface IPData {
+  status: string;
+  country: string;
+  countryCode: string;
+  region: string;
+  regionName: string;
+  city: string;
+  zip: string;
+  lat: number;
+  lon: number;
+  timezone: string;
+  isp: string;
+  org: string;
+  as: string;
+  query: string;
+}
+
+const ip = ref("");
+const ipData = ref<IPData>({} as IPData);
 const getIp = async () => {
   try {
     const res = await fetch("https://api.ipify.org?format=json");
     const data = await res.json();
     ip.value = data.ip;
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
-onMounted(() => {
-    getIp();
+const getInfo = async (ip: string) => {
+  try {
+    const res = await fetch(`http://ip-api.com/json/${ip}`);
+    const data = await res.json();
+    ipData.value = data;
+    console.log("🚀 ~ file: App.vue:18 ~ getInfo ~ data:", data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+onMounted(async () => {
+  await getIp();
+  await getInfo(ip.value);
   console.log(window.navigator);
-  
 });
 </script>
 <style>
@@ -27,9 +64,13 @@ body {
   height: 100vh;
   background-color: hsl(261, 100%, 50%);
   color: hsl(0, 0%, 100%);
-  line-height: 100vh;
   text-align: center;
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+td {
+  text-align: left;
 }
 </style>
